a7815f8938bbaa2caff955a8aa88ba28a3d94b5b
redkaio <chrno.92@gmail.com>
2021-06-01 10:14:54 -0300

    Commit inicial

---
+++ GameTOP.Lib/Jogador2.cs
@@ -0,0 +1,20 @@
+namespace GameTOP
+{
+    public class Jogador2 : iJogador
+    {
+        public string Chuta()
+        {
+            return "Cezar está chutando \n";
+        }
+
+        public string Corre()
+        {
+            return "Cezar está correndo \n";
+        }
+
+        public string Passe()
+        {
+            return "Cezar está passando \n";
+        }
+    }
+}
---
+++ GameTOP/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA(
+                
+                new Jogador1(),
+                new Jogador1("Day")
+                );
+            jogo.IniciarJogo();
+        }
+    }
+}
